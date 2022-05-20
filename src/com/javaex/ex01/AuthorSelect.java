8087a8777299b51b994753c236aabd31beaa9456
Garden22 <dalkomjjang@gmail.com>
2022-05-20 17:17:53 +0900

    220520

--- src/com/javaex/ex01/AuthorSelect.java
+++ src/com/javaex/ex01/AuthorSelect.java
@@ -6,10 +6,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class Update {
+public class AuthorSelect {
 
 	public static void main(String[] args) {
-
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
@@ -24,27 +23,30 @@ public class Update {
 			
 			// 3. SQL문 준비 / 바인딩 / 실행
 			String query = "";
-			query += "update author "; 
-			query += "set author_name = ?, ";
-			query += "author_desc = ? ";
-			query += "where author_id = ?";
+			query += "select author_id, author_name, author_desc\n"; 
+			query += "from author\n"; 
+
 			System.out.println(query);
 			
 			pstmt = conn.prepareStatement(query); // 문자열 쿼리로 만들기
-			pstmt.setString(1, "최문열");
-			pstmt.setString(2, "서울시");
-			pstmt.setInt(3, 1);
 			
-			int count = pstmt.executeUpdate();
-			
+			rs = pstmt.executeQuery();
+					
 			// 4.결과처리
-			System.out.println(count + "건이 등록되었습니다.");
-
+			while(rs.next()) {
+				int authorID = rs.getInt(1);
+				String authorName = rs.getString(2);
+				String authorDesc = rs.getString(3);
+			
+				System.out.println(authorID + ": " + authorName + ", " + authorDesc);
+			}
 			
 		} catch (ClassNotFoundException e) {
 			System.out.println("error: 드라이버 로딩 실패 - " + e);
+			
 		} catch (SQLException e) {
 			System.out.println("error:" + e);
+			
 		} finally {
 			// 5. 자원정리
 			try {
@@ -61,7 +63,5 @@ public class Update {
 				System.out.println("error:" + e);
 			}
 		}
-
 	}
-
 }
